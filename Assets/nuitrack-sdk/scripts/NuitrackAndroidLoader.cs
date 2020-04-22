d6eb31285f70f7de22eb9f365383971e5f980655
UomoCaffeLatte <nikhil.reji@live.co.uk>
2020-04-22 22:07:14 +0100

    first commit

---
+++ Assets/nuitrack-sdk/scripts/NuitrackAndroidLoader.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using System.Threading;
+
+public enum NuitrackInitState
+{
+	INIT_OK = 0,
+	INIT_NUITRACK_MANAGER_NOT_INSTALLED = 1,
+	INIT_NUITRACK_RESOURCES_NOT_INSTALLED = 2,
+	INIT_NUITRACK_SERVICE_ERROR = 3,
+	INIT_NUITRACK_NOT_SUPPORTED = 4
+}
+
+public class NuitrackAndroidLoader
+{
+    public static NuitrackInitState initState = NuitrackInitState.INIT_NUITRACK_NOT_SUPPORTED;
+    public static bool initComplete;
+    public static NuitrackInitState InitNuitrackLibraries()
+    {
+        #if UNITY_ANDROID // preprocessor macro - this code will only run on android
+            Debug.Log("InitNuitrackLibraries() -- starts.");
+
+            try 
+            {
+                initComplete = false;
+
+                // get current android context
+                AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+
+                // locate nuitrackhelperjar java class and allocate to javaNuitrackClass
+                AndroidJavaClass javaNuitrackClass = new AndroidJavaClass("com.tdv.nuitrack.sdk.Nuitrack");
+
+                // Load nuitrack libraries
+                javaNuitrackClass.CallStatic("init", jo, new NuitrackCallback());
+                
+                // wait for nuitrack libraries to load
+                while (!initComplete)
+                {
+                    Thread.Sleep(50);
+			    }
+            } 
+            catch (System.Exception ex) 
+            {
+                Debug.Log("Exception: " + ex);
+            }
+        #endif
+        return initState;
+    }
+
+}
+
+// implement callbacks based on template
+public class NuitrackCallback : AndroidJavaProxy 
+{
+    public NuitrackCallback() : base("com.tdv.nuitrack.sdk.Nuitrack$NuitrackCallback") { }
+
+    void onInitSuccess(AndroidJavaObject context)
+    {
+        Debug.Log("Nuitrack callback: onInitSuccess");
+        NuitrackAndroidLoader.initState = NuitrackInitState.INIT_OK;
+        NuitrackAndroidLoader.initComplete = true;
+    }
+
+    void onInitFailure(int errorId)
+    {
+        Debug.Log("Nuitrack callback: onInitFailure");
+        NuitrackAndroidLoader.initState = (NuitrackInitState)errorId;
+        NuitrackAndroidLoader.initComplete = true;
+    }
+}
---
+++ Assets/nuitrack-sdk/scripts/NuitrackManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using UnityEngine;
+using nuitrack;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class InitEvent : UnityEvent<NuitrackInitState>
+{
+}
+
+public class NuitrackManager : MonoBehaviour
+{
+    // [SerializeField]
+    // bool
+    // depthModuleOn = false,
+    // userTrackerModuleOn = false,
+    // skeletonTrackerModuleOn = true;
+
+    private bool firstTime = false; //in order to prevent double NuitrackInit() calls on startup (in Awake and in OnApplicationPause)
+    private static nuitrack.SkeletonTracker skeletonTracker;
+    public static nuitrack.SkeletonTracker SkeletonTracker {get {return skeletonTracker; }}
+    private NuitrackInitState initState = NuitrackInitState.INIT_NUITRACK_MANAGER_NOT_INSTALLED;
+    public NuitrackInitState InitState { get { return NuitrackAndroidLoader.initState; }}
+    static nuitrack.SkeletonData skeletonData;
+    public static nuitrack.SkeletonData SkeletonData { get { return skeletonData; } }
+    static nuitrack.Skeleton[] trackedSkeletons;
+    static nuitrack.Skeleton trackedSkeleton;
+
+    [SerializeField] InitEvent initEvent;
+    [SerializeField] bool runInBackground = false;
+
+    public Text numSkeletonText;
+
+    void Awake()
+    {
+        initState = NuitrackAndroidLoader.InitNuitrackLibraries();
+
+        if (initEvent != null)
+        {
+            initEvent.Invoke(initState);
+        }
+
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Application.targetFrameRate = 60;
+        Application.runInBackground = runInBackground;
+
+        if (initState == NuitrackInitState.INIT_OK)
+        {
+            NuitrackInit();
+        }
+    }
+
+    void NuitrackInit()
+    {
+        nuitrack.Nuitrack.Init();
+    
+        // Create and setup all required modules
+        skeletonTracker = nuitrack.SkeletonTracker.Create();
+
+        // Add event handlers for all modules created (Subscribing to the onSkeletonUpdateEvent)
+        skeletonTracker.OnSkeletonUpdateEvent += OnSkeletonUpdate;
+        
+        // Run Nuitrack. This starts sensor data processing
+        nuitrack.Nuitrack.Run();
+        Debug.Log("Run OK");
+    }
+
+    void FixedUpdate()
+    {
+        nuitrack.Nuitrack.Update();
+    }
+    void OnSkeletonUpdate(nuitrack.SkeletonData _skeletonData)
+    {
+        numSkeletonText.text = "Tracked skeletons:  LOL";
+
+        skeletonData = _skeletonData;
+
+        numSkeletonText.text = "Tracked skeletons: " + skeletonData.NumUsers.ToString();
+
+    
+    }
+
+}
